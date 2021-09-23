6e5a91bc43c5363abbe5675b31b1008b1e6591df
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2021-09-23 16:25:31 +0530

    Updated

--- Home/static-modal.php
+++ Home/static-modal.php
@@ -1,8 +1,8 @@
 <div class="container static-modal">
-    <div class="d-flex justify-content-center align-items-center col-md-12">
+    <div class="d-flex justify-content-center align-items-center col-md-12 static-modal-mobile">
 
         <div class="d-flex justify-content-center align-items-center m-3 static-modal-content col-md-4">
-            <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_9ycwmgb9.json" background="transparent" speed="1" class="lottie-customer" loop autoplay></lottie-player>
+            <lottie-player id="static-modal-lot" src="https://assets10.lottiefiles.com/packages/lf20_9ycwmgb9.json" background="transparent" speed="1" class="lottie-customer" loop autoplay></lottie-player>
             <div>
                 <h3>Groceries on MRP</h3>
                 <p>Buy your pantry essentials at MRP just as you would at your nearest Kirana store</p>
@@ -10,7 +10,7 @@
         </div>
 
         <div class="d-flex justify-content-center align-items-center m-3 static-modal-content col-md-4">
-            <lottie-player src="https://assets10.lottiefiles.com/private_files/lf30_y0m027fl.json" background="transparent" speed="1" class="lottie-customer" loop autoplay></lottie-player>
+            <lottie-player id="static-modal-lot" src="https://assets10.lottiefiles.com/private_files/lf30_y0m027fl.json" background="transparent" speed="1" class="lottie-customer" loop autoplay></lottie-player>
             <div>
                 <h3>60 Min Delivery</h3>
                 <p>Get your groceries delivered within 60 mins. No need to wait for a ‘slot’ or next day delivery</p>
@@ -18,7 +18,7 @@
         </div>
 
         <div class="d-flex justify-content-center align-items-center m-3 static-modal-content col-md-4">
-            <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_qX4zwY.json" background="transparent" speed="1" class="lottie-customer" loop autoplay></lottie-player>
+            <lottie-player id="static-modal-lot" src="https://assets10.lottiefiles.com/packages/lf20_qX4zwY.json" background="transparent" speed="1" class="lottie-customer" loop autoplay></lottie-player>
             <div>
                 <h3>Safety & Comfort</h3>
                 <p>Buy all your daily kitchen items without leaving home. Be safe, let YoPartners do your chore.</p>
