916032f944b6d4ead38355c17b3471b81b2a292c
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2021-09-30 11:53:54 +0530

    Updated

--- includes/tnc-content.php
+++ includes/tnc-content.php
@@ -4,4 +4,24 @@
         Please read the Terms of Use carefully before using the Platform or registering on the Platform or accessing any material or information through the Platform. By clicking on the "I Accept" button, You accept this Terms of Use and agree to be legally bound by the same.
         Use of and access to the Platform is offered to You upon the condition of acceptance of all the terms, conditions and notices contained in this Terms of Use and Privacy Policy, along with any amendments made by Pantryo at its sole discretion and posted on the Platform from time to time.
     </p>
+    <p>For the purposes of these Terms of Use, the term 'Pantryo' or 'Us' or 'We' refers to Gizmmo Alchemy. The term 'You' refers to the user or visitor of the Website and/or App. When You use our services, You will be subject to the terms, guidelines and policies applicable to such service and as set forth in these Terms of Use. As long as you comply with these Terms of Use, We grant You a personal, non-exclusive, non-transferable, limited privilege to enter and use our Platforms and services.</p>
+
+    <h2>PART A - GENERAL TERMS RELATING TO PANTRYO SERVICES</h2>
+
+    <h3>1. Registration:</h3>
+    <p>a. You shall be permitted to access the Platform, avail the Pantryo Services and connect with Merchants and Delivery Partners on the Platform only upon creating an Account (as defined below) and obtaining a registration on the Platform. Your ability to continue using the Platform, Pantryo Services is subject to Your continued registration on the Platform. You will be required to enter Your personal information including your name, contact details, address, valid phone number while registering on the Platform.</p>
+    <p>b. As a part of the registration, You may be required to undertake a verification process to verify Your personal information and setting up the Account.</p>
+    <p>c. Pantryo shall have the right to display the information, feedback, ratings, reviews etc. provided by You on the Platform. You agree and accept that as on the date of Your registration on the Platform, the information provided by You is complete, accurate and up-to-date. In the event of any change to such information, You shall be required to promptly inform Pantryo of the same, in writing, at least 1 (one) week prior to the date on which such change shall take effect. You acknowledge and accept that Pantryo has not independently verified the information provided by You. Pantryo shall in no way be responsible or liable for the accuracy or completeness of any information provided by You. If You provide any information that is untrue, inaccurate, not current or incomplete, or Pantryo has reasonable grounds to suspect that such information is untrue, inaccurate, not current or incomplete, Pantryo reserves the right to suspend or terminate Your Account (defined below) and refuse any and all current or future use of the Platform (or any portion thereof) at any time.</p>
+
+    <h3>2. Pantryo Services:</h3>
+    <p>a. The Platform provides You with the following services ("Pantryo Services"):</p>
+    <ol>
+        <li>It allows You to connect with Merchants and Delivery Partners;</li>
+        <li>It allows You to view the items/services ("Items") listed on the Platform by the Merchants;</li>
+        <li>It allows You to purchase Item(s) from the Merchants listed on the Platform and allows You to get the Items delivered to You through Delivery Partners ("Purchase Services");</li>
+        <li>It allows You to purchase Items from Non- Merchants and get the same delivered to You by the Delivery Partners ("Delivery Services");</li>
+        <li>It allows you to pick up- and drop off packages from 1 (one) location to the other through the Delivery Partner ("Pick Up and Drop Off Services");</li>
+        <li>It allows You to give ratings, write comments and reviews about Delivery Partners and Merchants;</li>
+        <li>It facilitates improvement in the quality of the services provided by Pantryo on the Platform based on User ratings, reviews and feedbacks provided on the Platform.</li>
+    </ol>
 </div>
