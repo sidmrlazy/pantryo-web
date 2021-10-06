22edd51efbb056e18d73d75cdaca3ff91b425d7e
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2021-10-06 15:50:03 +0530

    Updated Website with Shop Section

--- Shop/shop-section.php
+++ Shop/shop-section.php
@@ -1,26 +1,30 @@
-<!-- ======== Customer Animated Section Start  ======== -->
-<div class="animated-text-container container-fluid mt-5" id="about_us">
+<!-- ======== Shop Animated Section Start  ======== -->
+<div class="animated-text-container-shop container-fluid mt-5" id="about_us">
     <div class="d-flex justify-content-start align-items-center">
         <div class="d-flex justify-content-center align-items-center col-md-8">
             <div class="col-md-10">
-                <h1 id="shop-section-changing-text">zo-MAA-to ki kasam, Stay home</h1>
+                <h1 id="shop-section-changing-text">Apki Dukaan, Aapki Kamai</h1>
                 <!-- <h1 class="changing-content"></h1> -->
                 <h2>Become a registered <span>Yo</span> Store and start selling online. </h2>
                 <img src="assets/images/icons/gstore.png" />
             </div>
         </div>
-        <div class="col-md-4 d-flex justify-content-center align-items-center second-section">
-            <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_USXu1l.json" background="transparent" speed="1" class="lottie" loop autoplay></lottie-player>
+        <div class="col-md-4 d-flex justify-content-center align-items-center second-section-shop">
+            <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_USXu1l.json" background="transparent" speed="1" class="lottie-shop" loop autoplay></lottie-player>
         </div>
     </div>
 </div>
-<!-- ======== Customer Animated Section End  ======== -->
+<!-- ======== Shop Animated Section End  ======== -->
 
-<!-- ======== Customer Feature Section Start  ======== -->
-<div class="container-fluid" id="customer-feature-section">
-    <div class="d-flex justify-content-center align-items-center w-100 customer-section-heading">
+<!-- ======== Shop Feature Section Start  ======== -->
+<div class="container-fluid" id="shop-feature-section">
+    <!-- <div class="d-flex justify-content-center align-items-center w-100 shop-section-heading">
         <h1>Online <span>Grocery</span> Home Delivery in <span>Lucknow</span></h1>
-    </div>
-    <?php include 'Home/static-modal.php'; ?>
+    </div> -->
+    <?php include 'Shop/shop-static-modal.php'; ?>
 </div>
-<!-- ======== Customer Feature Section End  ======== -->
+<!-- ======== Shop Feature Section End  ======== -->
+
+<!-- ======== Shop Job Details Start  ======== -->
+<!-- ======== Shop Job Details End  ======== -->
+
---
+++ Shop/shop-static-modal.php
@@ -0,0 +1,28 @@
+<div class="container static-modal">
+    <div class="d-flex justify-content-center align-items-center col-md-12 static-modal-mobile">
+
+        <div class="d-flex justify-content-center align-items-center m-3 static-modal-content col-md-4">
+            <lottie-player id="static-modal-lot" src="https://assets10.lottiefiles.com/packages/lf20_9ycwmgb9.json" background="transparent" speed="1" class="lottie-customer" loop autoplay></lottie-player>
+            <div>
+                <h3>No profit sharing</h3>
+                <p>PantrYo does not take ANY percentage of your revenue through it’s platform</p>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-center align-items-center m-3 static-modal-content col-md-4">
+            <lottie-player id="static-modal-lot" src="https://assets10.lottiefiles.com/private_files/lf30_y0m027fl.json" background="transparent" speed="1" class="lottie-customer" loop autoplay></lottie-player>
+            <div>
+                <h3>Daily Orders</h3>
+                <p>Get Orders from customers Daily Online through PantrYO</p>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-center align-items-center m-3 static-modal-content col-md-4">
+            <lottie-player id="static-modal-lot" src="https://assets10.lottiefiles.com/packages/lf20_qX4zwY.json" background="transparent" speed="1" class="lottie-customer" loop autoplay></lottie-player>
+            <div>
+                <h3>Your Safety is important too </h3>
+                <p>Avoid crowding at your shop, take orders online and hand them over to us to deliver, at no added cost.</p>
+            </div>
+        </div>
+    </div>
+</div>
