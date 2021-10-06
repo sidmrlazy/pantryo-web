f84f1cfdfccc8936155ffcd0442322ef90a261b6
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2021-10-06 18:09:02 +0530

    Updated anchor tag on Logo

--- includes/navigation.php
+++ includes/navigation.php
@@ -14,7 +14,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">
+    <a class="navbar-brand" href="index.php">
       <img src="assets/images/logo/PantryoLogo.png" alt="PantryoLogo" />
     </a>
     <button
